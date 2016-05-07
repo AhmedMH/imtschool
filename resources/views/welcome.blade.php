ec692e93cbaa03fc951d6efadc3f62d0b8537d8c
Ahmed <enghamed126@gmail.com>
2016-05-07 02:25:30 +0200

    updated favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
 
     <!-- Favicons
     ================================================== -->
-    <link rel="shortcut icon" href="http://faviconist.com/icons/be4687b01285b518d14add81dff8d9ab/favicon.ico" />
+    <link rel="shortcut icon" href="{{asset('favicon.ico')}}" />
     <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
     <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
     <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
